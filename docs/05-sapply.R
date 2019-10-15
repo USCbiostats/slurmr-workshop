6f836edd68e2447d6ae0fcedfc15654bd6636d4f
George G. Vega Yon <g.vegayon@gmail.com>
2019-10-14 18:27:39 -0700

    Updating logs

--- docs/05-sapply.R
+++ docs/05-sapply.R
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=sapply
+#SBATCH --job-name=sapply-sourceSlurm
 #SBATCH --time=01:00:00
 
 # Model parameters
@@ -18,5 +18,5 @@ simpi <- function(i) {
 set.seed(12322)
 ans <- sapply(1:nsims, simpi)
 
-saveRDS(ans, "01-sapply.rds")
+saveRDS(ans, "05-sapply.rds")
 
